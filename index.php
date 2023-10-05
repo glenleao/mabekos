3028c3b35e5cdc4bb8e3311edacdcf8190c937c3
Glen Leão <glenleao@gmail.com>
2023-10-05 14:10:12 -0300

    gitignore

--- index.php
+++ index.php
@@ -2,14 +2,20 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Mabeko\Page;
+
+$app = new Slim();
 $app->config('debug', true);
 $app->get('/', function(){
 // echo "Olá Mabekos!";
-$sql = new Mabeko\DB\Sql();
-$results = $sql->select("SELECT * FROM tb_users");	
+// $sql = new Mabeko\DB\Sql();
+// $results = $sql->select("SELECT * FROM tb_users");	
 
-echo json_encode($results);
+// echo json_encode($results);
+
+	$page = new Page();
+	$page->setTpl("index");
 
 });
 
